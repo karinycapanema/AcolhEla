9711344bed47b467f49c4e9540e0698eb1f2c2e4
juliabittencourt30 <bittjuliarosa@gmail.com>
2026-08-28 12:20:06 -0300

    ajustes finais

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import VoluntarioView from '../views/voluntario/VoluntarioView.vue'
+import VoluntarioView from '../views/VoluntarioView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
   routes: [
-    {
-      path: '/',
-      redirect: '/voluntario'
-    },
     {
       path: '/voluntario',
       name: 'VoluntarioView',
