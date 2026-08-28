cb66cf8a3b8c76d4187d416f5d64dfbd5ab983a8
karinycapanema <capanemakariny@gmail.com>
2026-08-27 21:13:25 -0300

    criação cadastro usuario

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserClock, faEnvelope, faPhone, faLock, faUnlock } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserClock, faEnvelope, faPhone, faLock,  faUnlock)
 
 const app = createApp(App)
+
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 app.use(router)
 
